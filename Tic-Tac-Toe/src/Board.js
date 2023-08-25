1c93c3ecd9262167231dcf9940fbafcd1487a4c9
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-25 12:02:55 +0700

    Use two loops to make the squares instead of hardcoding them.

--- Tic-Tac-Toe/src/Board.js
+++ Tic-Tac-Toe/src/Board.js
@@ -12,24 +12,18 @@ export default function Board({ xIsNext, squares, onPlay }) {
         status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
+    const rows = [0, 1, 2].map((row) => {
+        const cols = [0, 1, 2].map((col) => {
+            const index = row * 3 + col;
+            return <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)}/>
+        });
+        return <div key={row} className="board-row">{cols}</div>
+    });
+
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
+            {rows}
         </>
     )
 
