e4d4fe13a0292b79eb9248e06bbb9387e09c0a5f
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-25 11:18:23 +0700

    useState the X

--- Tic-Tac-Toe/src/Square.js
+++ Tic-Tac-Toe/src/Square.js
@@ -1,6 +1,15 @@
-export default function Square({value, onClick}) {
+import {useState} from "react";
+
+export default function Square({onClick}) {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        console.log("clicked : ", value);
+        setValue("X");
+    }
+
     return (
-        <button className="square" onClick={onClick}>
+        <button className="square" onClick={handleClick}>
             {value}
         </button>
     )
